db5ce414d1de5804415207298c151df450cff5a0
Balint Molnar <balintkilencvenegy@gmail.com>
2020-09-23 15:25:29 +0200

    Add base components to work with

--- trefl/src/App.js
+++ trefl/src/App.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Footer from "./components/layout/Footer"
+import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/Home";
+import ThemeContext from "./components/context/ThemeContext";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  const accessToken = "8RYlIatUUjxLOhPVAz22a6pVEYhePGXdjwiwToaJKDI";
-  const apiUrl = `https://trefle.io/api/v1/divisions?token=${accessToken}`;
+  // const accessToken = "8RYlIatUUjxLOhPVAz22a6pVEYhePGXdjwiwToaJKDI";
+  // const apiUrl = `https://trefle.io/api/v1/divisions?token=${accessToken}`;
+
+  const themeHook = useState("light");
 
   return (
-    <div className="App">
-      
-      <Router>
-        <Navbar/>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-        <Footer></Footer>
-      </Router>
-    </div>
+    <ThemeContext.Provider value={themeHook}>
+      <div className="App">
+        <Header />
+        <Main />
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+          <Footer></Footer>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
---
+++ trefl/src/components/ThemeToggler.js
@@ -0,0 +1,23 @@
+import React, { useState, useContext } from "react";
+import ThemeContext from "./context/ThemeContext";
+
+const ThemeTogglerStyle = {
+  cursor: "pointer",
+};
+
+const ThemeToggler = () => {
+  const [themeMode, setThemeMode] = useContext(ThemeContext);
+
+  return (
+    <div
+      style={ThemeTogglerStyle}
+      onClick={() => {
+        setThemeMode(themeMode === "light" ? "dark" : "light");
+      }}
+    >
+      <span title="switch theme">{themeMode === "light" ? "🌙" : "☀️"}</span>
+    </div>
+  );
+};
+
+export default ThemeToggler;
