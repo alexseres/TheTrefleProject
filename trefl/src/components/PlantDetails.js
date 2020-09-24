0a30f29312a3046805782cabc8c9422b7fe86a7a
Balint Molnar <balintkilencvenegy@gmail.com>
2020-09-24 10:08:33 +0200

    Add minor polish

--- trefl/src/components/PlantDetails.js
+++ trefl/src/components/PlantDetails.js
@@ -1,37 +1,36 @@
 ﻿import React, { useState, useEffect } from "react";
 
 const PlantDetails = () => {
-    const [plantData, setPlantData] = useState([]);
+  const [plantData, setPlantData] = useState([]);
 
-    useEffect(() => {
-        fetch('https://trefle.io/api/v1/plants/183086?token=RHxIcD6YqLzQoeCHuyF3ur8AFaEiGwknPb34ufApQNM')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Failed to fetch.');
-                }
-                return response.json();
-            })
-            .then(plantData => {
-                const plantDetail = plantData.data;
+  useEffect(() => {
+    fetch(
+      "https://trefle.io/api/v1/plants/183086?token=RHxIcD6YqLzQoeCHuyF3ur8AFaEiGwknPb34ufApQNM"
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch.");
+        }
+        return response.json();
+      })
+      .then((plantData) => {
+        const plantDetail = plantData.data;
 
-                setPlantData(
-                    plantData = plantDetail
-                );
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }, []);
+        setPlantData((plantData = plantDetail));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return (
-        <div>
-            <h1>{plantData.common_name}</h1>
-            <p>{plantData.scientific_name}</p>
-            <p>{plantData.family_common_name}</p>
-            <img src={plantData.image_url} alt={plantData.common_name}/>
-        </div>
-    );
-    
+  return (
+    <div>
+      <h1>{plantData.common_name}</h1>
+      <p>{plantData.scientific_name}</p>
+      <p>{plantData.family_common_name}</p>
+      <img src={plantData.image_url} alt={plantData.common_name} />
+    </div>
+  );
 };
 
 export default PlantDetails;
